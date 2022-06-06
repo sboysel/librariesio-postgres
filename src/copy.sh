e0c4db2b41844a92a4320db17ddc213356f5b934
Sam Boysel <sboysel@gmail.com>
2022-06-06 03:35:09 -0700

    load CSV into postgres if the table is empty

--- src/copy.sh
+++ src/copy.sh
@@ -4,6 +4,10 @@ export PGPASSWORD=postgres
 pg="psql -U postgres -d librariesio -h postgres"
 
 for item in "projects_with_repository_fields" "dependencies" "versions"; do
-    echo "Copy $item..."
-    $pg -c "\COPY $item FROM './data/$item.csv' DELIMITER ',' CSV;"
+    rows=$($pg -t -c "SELECT COUNT(*) FROM $item;")
+    if [ "$rows" -eq "0" ]
+    then
+        echo "Copy $item..."
+        $pg -c "\COPY $item FROM './data/$item.csv' DELIMITER ',' CSV;"
+    fi
 done
