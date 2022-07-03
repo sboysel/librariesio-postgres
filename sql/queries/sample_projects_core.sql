7a6597ade66dec1af9aeab776f04f32af88428af
Sam Boysel <sboysel@gmail.com>
2022-07-03 03:33:51 -0700

    sample core projects by popularity

--- sql/queries/sample_projects_core.sql
+++ sql/queries/sample_projects_core.sql
@@ -8,25 +8,36 @@ FROM projects_with_repository_fields;
 
 /* Top quartile of projects ranked by total number of dependents as of 2020-01-12 */
 CREATE TEMP TABLE tmp AS
-  (SELECT name,
+  (SELECT id,
+          name,
+          repository_name_with_owner,
+          repository_stars_count,
           dependent_repositories_count,
           repository_url
    FROM
      (SELECT *,
              PERCENT_RANK() OVER (
-                                  ORDER BY dependent_repositories_count) AS r
+                                  ORDER BY repository_stars_count) AS r
       FROM
-        (SELECT DISTINCT ON(repository_url, dependent_repositories_count) *
-         FROM projects_with_repository_fields) j) i
-   WHERE r >= 0.25
-     AND dependent_repositories_count IS NOT NULL
-   ORDER BY dependent_repositories_count DESC);
+        (SELECT DISTINCT ON(repository_url, repository_stars_count) *
+         FROM projects_with_repository_fields
+         WHERE repository_stars_count IS NOT NULL
+           AND dependent_repositories_count IS NOT NULL
+           AND repository_stars_count >= 100
+           AND dependent_repositories_count >= 1
+           AND repository_fork = FALSE
+           AND repository_license IS NOT NULL
+           AND repository_readme_filename IS NOT NULL
+           AND repository_sourcerank IS NOT NULL
+           AND repository_host_type = 'GitHub') j) i
+   WHERE r >= 0.75
+   ORDER BY repository_stars_count DESC);
 
 /* Count of top quartile dependencies */
 SELECT COUNT(*)
 FROM tmp;
 
-/* 0.1% Bernoulli random sample of top quartile dependents */
+/* 1% Bernoulli random sample of top quartile dependents */
 CREATE TABLE projects_core AS
   (SELECT *
-   FROM tmp TABLESAMPLE BERNOULLI (0.1) REPEATABLE(100));
+   FROM tmp TABLESAMPLE BERNOULLI (1) REPEATABLE(100));
